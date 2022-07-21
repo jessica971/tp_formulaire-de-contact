683e3150b735ba03c8c9b2b16f2475d4a4c92f82
Kevin97125 <108547007+Kevin97125@users.noreply.github.com>
2022-07-21 09:40:49 -0400

    mode debug
    mode debug

---
+++ www/index.php
@@ -0,0 +1,58 @@
+<?php 
+define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
+// var_dump($_SERVER);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+} else {
+    if (IS_DEBUG) {
+        echo "Pas de POST";
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>TP Discord</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css" media="all and (max-width: 768px)">
+</head>
+
+<body>
+    <div id="formulaire">
+        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]?>">
+            <input type="text" placeholder="Prenom" name="firstname" required>
+            <input type="text" placeholder="Nom" name="lastname" required>
+            <input type="text" placeholder="Sujet" name="subject" required>
+            <input type="text" placeholder="exemple@gmail.com" name="email" required>
+            <textarea name="message" id="" cols="30" rows="10"></textarea>
+            <!-- <div id="select"> 
+            <select name="date">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <select name="month">
+                <option>Janviers</option>
+                <option>Février</option>
+                <option>Mars</option>
+                <option>Avril</option>
+                <option>Mai</option>
+            </select>
+            <select>
+                <option>2021</option>
+                <option> 2022</option>
+                <option> 2023</option>
+                <option> 2024</option>
+                <option> 2025</option>
+            </select>
+            </div> -->
+            <input type="submit" value="ENVOYER">
+        </form>
+    </div>
+</body></html>
