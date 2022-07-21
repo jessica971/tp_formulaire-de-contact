074d43404adcbec5dfc95cbe7826567884ad17fc
Kevin97125 <108547007+Kevin97125@users.noreply.github.com>
2022-07-21 12:30:47 -0400

    email
    verification email

--- www/index.php
+++ www/index.php
@@ -2,6 +2,7 @@
 define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
 
 $firstname = $lastname = $subject = $email = $message = ""; //initialise la variable a 0
+$firstnameError = $lastnameError = $subjectError = $emailError = $messageError = ""; 
 
 //Les variables PHP $_GET et $_POST sont utilisées pour collectées les données postées par les formulaires. $_GET et $_POST sont des tableaux associatifs contenant des paires clé/valeur où les clés sont les noms des contrôles de formulaires (name="...") et les valeurs sont les données d'entrée de l'utilisateur.
 
@@ -9,10 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //$_SERVER : ce sont des valeurs uti
     if(IS_DEBUG){
         echo "POST";
     }
-    $firstname = isset($_POST["firstname"]) ? checkInput($_POST["firstname"]) : "";
+    $firstname = isset($_POST["firstname"]) ? checkInput($_POST["firstname"]) : ""; //La fonction isset() est une fonction intégrée en PHP qui vérifie si une variable est définie, ce qui signifie qu’elle doit être déclarée et non NULL. Cette fonction renvoie TRUE si la variable existe et n’est pas NULL, sinon elle renvoie FALSE.
     $lastname = isset($_POST["lastname"]) ? checkInput($_POST["lastname"]) : "";
     $subject = isset($_POST["subject"]) ? checkInput($_POST["subject"]) : "";
     $email = isset($_POST["email"]) ? checkInput($_POST["email"]) : "";
+    if (!isEmail($email)) {//condition envoi le message qui est contenu dans $emailError si l'email est pas bon.
+        $emailError = "Veuillez vérifier votre email;";
+    }
     $message = isset($_POST["message"]) ? checkInput($_POST["message"]) : "";
 } else {
     if (IS_DEBUG) {
@@ -20,11 +24,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //$_SERVER : ce sont des valeurs uti
     }
 }
 
-function checkInput($input){
-    $input = trim($input);
-    $input = stripslashes($input);
-    $input = htmlspecialchars($input);
+function checkInput($input){ //fonction qui vérifie les Input
+    $input = trim($input); //retourne la chaîne str, après avoir supprimé les caractères invisibles en début et fin de chaîne. Si le second paramètre charlist est omis, trim() supprimera les caractères suivants :
+    $input = stripslashes($input); //Supprime les antislashs d'une chaîne
+    $input = htmlspecialchars($input); //Convertit les caractères spéciaux en entités HTML
+    if (IS_DEBUG) {
+        echo $input;
+        echo "<br>";
+    }
     return $input;
+}
+
+function isEmail($email){// fonction qui verifie l'email
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 ?>
 
@@ -40,13 +52,19 @@ function checkInput($input){
 
 <body>
     <div id="formulaire">
-        <form method="post" action="<?php echo htmlspecialchars() $_SERVER["PHP_SELF"]?>">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
             <input type="text" placeholder="Prenom" name="firstname" value="<?php echo $firstname ?>" required>
             <!-- les variable sont appeler par l'attribut name -->
             <!-- //value permet de afficher ce que l'utilisateur a tapez -->
             <input type="text" placeholder="Nom" name="lastname" value="<?php echo $lastname ?>" required>
             <input type="text" placeholder="Sujet" name="subject" value="<?php echo $subject ?>" required>
-            <input type="text" placeholder="exemple@gmail.com" name="email" value="<?php echo $email ?>" required>
+            <input type="email" placeholder="exemple@gmail.com" name="email" value="<?php echo $email ?>" required>
+            <?php 
+                if ($emailError != "") {
+                    echo '<p class="error">' . $emailError . '</p>';
+                }
+            ?>
+            <!-- <p class="error">Veuillez vérifier votre email</p> -->
             <textarea name="message" placeholder="Tapez votre meessage" cols="30" rows="10" require>
             <?php echo $message ?> </textarea>
             <!-- <div id="select"> 
