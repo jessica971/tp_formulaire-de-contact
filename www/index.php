cc4ba2194b17263d0219e11f9bdc7a1ff21786b5
Kevin97125 <108547007+Kevin97125@users.noreply.github.com>
2022-07-21 11:43:25 -0400

    Update index.php
    sécurité du formulaire

--- www/index.php
+++ www/index.php
@@ -1,14 +1,31 @@
 <?php 
 define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
-// var_dump($_SERVER);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+$firstname = $lastname = $subject = $email = $message = ""; //initialise la variable a 0
+
+//Les variables PHP $_GET et $_POST sont utilisées pour collectées les données postées par les formulaires. $_GET et $_POST sont des tableaux associatifs contenant des paires clé/valeur où les clés sont les noms des contrôles de formulaires (name="...") et les valeurs sont les données d'entrée de l'utilisateur.
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { //$_SERVER : ce sont des valeurs utiles que nous donne le serveur. Pour les afficher, il faut indiquer ce qu'on demande entre crochets puisque que c'est un array.
+    if(IS_DEBUG){
+        echo "POST";
+    }
+    $firstname = isset($_POST["firstname"]) ? checkInput($_POST["firstname"]) : "";
+    $lastname = isset($_POST["lastname"]) ? checkInput($_POST["lastname"]) : "";
+    $subject = isset($_POST["subject"]) ? checkInput($_POST["subject"]) : "";
+    $email = isset($_POST["email"]) ? checkInput($_POST["email"]) : "";
+    $message = isset($_POST["message"]) ? checkInput($_POST["message"]) : "";
 } else {
     if (IS_DEBUG) {
         echo "Pas de POST";
     }
 }
 
+function checkInput($input){
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+}
 ?>
 
 <!doctype html>
@@ -23,12 +40,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body>
     <div id="formulaire">
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]?>">
-            <input type="text" placeholder="Prenom" name="firstname" required>
-            <input type="text" placeholder="Nom" name="lastname" required>
-            <input type="text" placeholder="Sujet" name="subject" required>
-            <input type="text" placeholder="exemple@gmail.com" name="email" required>
-            <textarea name="message" id="" cols="30" rows="10"></textarea>
+        <form method="post" action="<?php echo htmlspecialchars() $_SERVER["PHP_SELF"]?>">
+            <input type="text" placeholder="Prenom" name="firstname" value="<?php echo $firstname ?>" required>
+            <!-- les variable sont appeler par l'attribut name -->
+            <!-- //value permet de afficher ce que l'utilisateur a tapez -->
+            <input type="text" placeholder="Nom" name="lastname" value="<?php echo $lastname ?>" required>
+            <input type="text" placeholder="Sujet" name="subject" value="<?php echo $subject ?>" required>
+            <input type="text" placeholder="exemple@gmail.com" name="email" value="<?php echo $email ?>" required>
+            <textarea name="message" placeholder="Tapez votre meessage" cols="30" rows="10" require>
+            <?php echo $message ?> </textarea>
             <!-- <div id="select"> 
             <select name="date">
                 <option>1</option>
@@ -55,4 +75,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="submit" value="ENVOYER">
         </form>
     </div>
-</body></html>
+</body>
+
+</html>
